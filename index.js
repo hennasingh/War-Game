9fe462fa75e77a638435db67d46a8b8b3594319e
Henna Singh <hennasingh04@gmail.com>
2023-11-09 21:05:16 +0000

    added logic to find the winner of the two cards

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-
+const newCards = document.getElementById('new-deck')
+const drawCards = document.getElementById('draw-cards')
+const remainingCards = document.getElementById('remaining-cards')
 let deckId
+const cards = {
+    'ACE': 14,
+    'KING' :13,
+    'QUEEN': 12,
+    'JACK': 11,
+    '10':10,
+    '9':9,
+    '8':8,
+    '7':7,
+    '6':6,
+    '5':5,
+    '4':4,
+    '3':3,
+    '2':2,
+    '1':1
+}
+
  function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res => res.json())
     .then(data => {
         console.log(data)
         deckId = data.deck_id
+        remainingCards.textContent = data.remaining
     })
 }
 
@@ -14,19 +34,34 @@ function generateCards() {
     .then(res => res.json())
     .then(data => {
         console.log(data.cards)
+
+        remainingCards.textContent = data.remaining
         const container = document.getElementById('cards')
-        
+
         container.children[0].innerHTML = `
         <img src=${data.cards[0].image} class="card"/>
         `
         container.children[1].innerHTML = `
         <img src=${data.cards[1].image} class="card"/>
         `
+        document.getElementById('winner').textContent = findWinner(data.cards[0], data.cards[1])
 
+        if(data.remaining == 0){
+            drawCards.disabled = true
+        }
 
     })
 }
+function findWinner(card1, card2) {
+    if (cards[card1.value] > cards[card2.value]) {
+        return "Computer wins"
+    } else if(cards[card1.value] < cards[card2.value]) {
+        return "You win"
+    } else {
+        return "WAR!"
+    }
+}
 
-document.getElementById("new-deck").addEventListener('click', handleClick)
-document.getElementById('draw-cards').addEventListener('click', generateCards)
+newCards.addEventListener('click', handleClick)
+drawCards.addEventListener('click', generateCards)
 
